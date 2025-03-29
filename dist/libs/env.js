8236f2523841ff367954b9f1feefaf9bfc66846d
Divyansh Goel <divyanshgoel8860@gmail.com>
2025-03-29 10:53:20 +0530

    Deleted Dist Folder

--- dist/libs/env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NODE_ENV = exports.DATABASE_URL = void 0;
-exports.DATABASE_URL = 'postgresql://uber-clone_owner:npg_ZsQydI6Ai7wk@ep-soft-block-a54mbk2a-pooler.us-east-2.aws.neon.tech/uber-clone?sslmode=require';
-exports.NODE_ENV = 'production';
